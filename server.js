d835c1cbc6126b91d626e0c68f2bb04f92b62775
Erik Portin <erikportin@gmail.com>
2017-04-30 10:54:43 +0200

    express dev server

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config');
+
+const app = express();
+const compiler = webpack(webpackConfig);
+
+app.use(webpackDevMiddleware(compiler, {
+    publicPath: '/'
+}));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+app.listen(8080, function () {
+    console.log('Listening on port 8080');
+});
